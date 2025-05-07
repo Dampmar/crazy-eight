6fdd6aa6eb953b2a329de85bce7ab89418d5efa0
Dampmar <dampmarin@gmail.com>
2025-05-07 10:42:32 -0600

    first draft

---
+++ program/src/App.java
@@ -0,0 +1,8 @@
+import classes.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Game.initializeGame("testGame");
+    }
+}
