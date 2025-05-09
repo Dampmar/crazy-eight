290386189f39e84572a967eddb736e6473ffcea5
Daniel Marin <dampmarin@gmail.com>
2025-05-08 19:15:45 -0600

    reset game

--- src/src/App.java
+++ src/src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        // Game.init("game1");
-        Game game = new Game("game1");
-        // game.addUser("user1");
-        // game.removeUser("user2");
-        game.startGame();
+        // Game.init("game1");  // Uncomment to initialize a new game
+        Game game = new Game("game1"); 
+        // game.addUser("user1"); // Uncomment to add a user
+        // game.removeUser("user2"); // Uncomment to remove a user
+        // game.startGame(); // Uncomment to start the game
     }
 }
